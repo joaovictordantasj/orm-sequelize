f5f65297620000dbb749e196c7e54feb2d37bbcf
João Victor <joaovictordjardim@gmail.com>
2021-03-21 14:37:59 -0300

    Metodo criaPessoa criado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,6 +2,7 @@ const database = require('../models');
 
 class PessoaController {
   static async pegaTodasAsPessoas(req, res) {
+    // READ todos os registros
     try {
       const todasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(todasAsPessoas);
@@ -11,14 +12,26 @@ class PessoaController {
   }
 
   static async pegaUmaPessoa(req, res) {
+    // READ apenas um registro
     const { id } = req.params;
 
     try {
       const umaPessoa = await database.Pessoas.findOne({
         where: { id: Number(id) },
       });
-
       return res.status(200).json(umaPessoa);
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+
+  static async criaPessoa(req, res) {
+    // CREATE uma nova pessoa no banco
+    const novaPessoa = req.body;
+
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
     } catch (err) {
       return res.status(500).json(err.message);
     }
